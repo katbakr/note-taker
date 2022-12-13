5a18f160fb3386955282e2c45500556acb8fba5b
Katrina Baker <katrinamourine@gmail.com>
2022-12-12 21:42:06 -0800

    adding middleware code and post routw

--- server.js
+++ server.js
@@ -3,6 +3,9 @@ const express = require('express');
 const app = express();
 const PORT = 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
 const notes = require('./db/db');
 
 app.use(express.static('public'));
@@ -11,6 +14,12 @@ app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
+app.post('/api/notes', (req, res) => {
+  console.log(req.body);
+  res.json(req.body);
+})
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}!`);
 });
+
