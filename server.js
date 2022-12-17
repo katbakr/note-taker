47365d0f744ebed9232f4c6268c2bef4966f3d98
Katrina Baker <katrinamourine@gmail.com>
2022-12-17 13:20:23 -0800

    function to write notes and updated POST

--- server.js
+++ server.js
@@ -1,25 +1,43 @@
+const PORT = process.env.PORT || 3001
 const express = require('express');
+const fs = require('fs');
+const path = require('path');
 
 const app = express();
-const PORT = 3001;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+// const PORT = 3001;
 
 const notes = require('./db/db');
 
-app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static('public')); 
+
+ function makeNewNote(body, noteArray) {
+   const newNote = body;
+   noteArray.push(newNote);
+   fs.writeFileSync(
+     path.join(__dirname, './db/db.json'),
+     JSON.stringify({ notes: noteArray}, null, 2),
+    
+   );
+   return newNote;
+ } 
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
 app.post('/api/notes', (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
+  // console.log(req.body);
+  // res.json(req.body);
+  req.body.id = notes.length.toString();
+//unique ID 
+  const NewNote = makeNewNote(req.body, notes);
+
+  res.json(newNote);
 })
 
 app.listen(PORT, () => {
-  console.log(`Server listening on port ${PORT}!`);
+  console.log(`Listening on port ${PORT}!`);
 });
 
